de358d2912cf9f8c599ead011b87ea345f5ab78d
olaoluwa siyanbola <ola@olaoluwas-MacBook-Pro.local>
2015-10-01 09:24:43 -0500

    fixed my code, added strong to p tag

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,73 +1,73 @@
 
-function outputLowBudgetMovies2HTML(movies){
-  var filteredMovies = movies.filter(function(movie){
-      return (movie.budget)=== 55;
-  });
-  var lowBudgetTitles = filteredMovies.map(function(movie){
-    return (movie.title)
-  })
-
-  lowBudgetTitles.forEach(function(title){
-    $(".low-budget-movies").append("<p>" + title + "</p>")
-   })
-}
-
-$(window).ready(function(){
-
-  $("button").on(  "click", function(){
-
-    $(".low-budget-movies").html("")
-
-    $.getJSON("/movies.json").then(function(data){
-      var movies = data.movies;
-
-      outputLowBudgetMovies2HTML(movies)
-
-
-
-      var leoWithMovies = _.filter(movies, function(movie){
-        return _.contains(movie.stars, 'Leonardo DiCaprio');
-
-      });
-      var leoMovieTitles = leoWithMovies.map(function(leoMovieTitle){
-        return (leoMovieTitle.title)
-      })
-
-      leoMovieTitles.forEach(function(title){
-        $(".leo-movies").append("<p>" + title + "</p>")
-       })
-
-
-
-    })
-
-  })
-
-
-});
-
-//  function groupedMovies(container, movies) {
-// $(container).html("");
-//   movies.forEach(function(movies) {
-//   $(container).append(
-//     '<div class="movie"><p>' + movie.title + '</p></div>'
-//   );
+// function outputLowBudgetMovies2HTML(movies){
+//   var filteredMovies = movies.filter(function(movie){
+//       return (movie.budget)=== 55;
+//   });
+//   var lowBudgetTitles = filteredMovies.map(function(movie){
+//     return (movie.title)
+//   })
 //
-// });
+//   lowBudgetTitles.forEach(function(title){
+//     $(".low-budget-movies").append("<p>" + title + "</p>")
+//    })
 // }
 //
-// $(document).ready(function() {
-//  $('.btn').on('click', function() {
-//    $.getJSON('/movies.json'), (function(data) {
-//      var low = _.where(data.movies, { budget: 55 });
-//      groupedMovies('.low-budget-movies', low);
+// $(window).ready(function(){
 //
-//      var leo = _.filter(data.movies, function(movie) {
-//        return _.contains(movies.stars, 'Leonardo DiCaprio');
-//      });
-//      groupedMovies('.leo-movies', leo);
+//   $("button").on(  "click", function(){
 //
-//    });
-//  });
+//     $(".low-budget-movies").html("")
+//
+//     $.getJSON("/movies.json").then(function(data){
+//       var movies = data.movies;
+//
+//       outputLowBudgetMovies2HTML(movies)
+//
+//
+//
+//       var leoWithMovies = _.filter(movies, function(movie){
+//         return _.contains(movie.stars, 'Leonardo DiCaprio');
+//
+//       });
+//       var leoMovieTitles = leoWithMovies.map(function(leoMovieTitle){
+//         return (leoMovieTitle.title)
+//       })
+//
+//       leoMovieTitles.forEach(function(title){
+//         $(".leo-movies").append("<p>" + title + "</p>")
+//        })
+//
+//
+//
+//     })
+//
+//   })
+//
 //
 // });
+
+ function groupedMovies(container, movies) {
+$(container).html("");
+  movies.forEach(function(movie) {
+  $(container).append(
+    '<div class="movie"><p>' + movie.title + '</p></div>'
+  );
+
+});
+}
+
+$(window).ready(function() {
+ $('button').on('click', function() {
+   $.getJSON('/movies.json', function(data) {
+     var low = _.where(data.movies, { budget: 55 });
+     groupedMovies('.low-budget-movies', low);
+
+     var leo = _.filter(data.movies, function(movie) {
+       return _.contains(movie.stars, 'Leonardo DiCaprio');
+     });
+     groupedMovies('.leo-movies', leo);
+
+   });
+ });
+
+});
